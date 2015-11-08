ab03a6ce5a383decef672c2ed11c7421c645df89
Rebecca Qu <beks.qu@gmail.com>
2015-11-08 10:27:44 -0500

    added notify me button on today view

--- Moneta/app/assets/javascripts/reminders.js
+++ Moneta/app/assets/javascripts/reminders.js
@@ -1,28 +1,33 @@
 $(document).on('ready page:load', function(){
-    var notice = new PNotify({
-      title: 'Notice- Groceries',
-      text: 'Reminder - Go to Loblaws for apples.'
-    });
-    notice.get().click(function(e) {
-      if ($(e.target).is('.ui-pnotify-closer *, .ui-pnotify-sticker * ')) return;
-      notice.update({
-          type: 'success',
-          text: 'Task Complete?!<div style="text-align: center;"></div>'
-      });
-    notice.get().click(function() {
-      notice.remove();
-    });
 
-    var stickyNotice = new PNotify({
-      title: 'Sticky Notice',
-      text: 'Check me out! I\'m a sticky notice. You\'ll have to close me yourself.',
-      hide: false
-    });
-    notice.get().click(function() {
-      if ($(e.target).is('.ui-pnotify-closer *, .ui-pnotify-sticker * ')) return;
-      notice.remove();
-    })
+    // var notice = new PNotify({
+    //   title: 'Notice- Groceries',
+    //   text: 'Reminder - Go to Loblaws for apples.'
+    // });
+
+    // notice.get().click(function(e) {
+    //   if ($(e.target).is('.ui-pnotify-closer *, .ui-pnotify-sticker * ')) return;
+    //   notice.update({
+    //       type: 'success',
+    //       text: 'Task Complete?!<div style="text-align: center;"></div>'
+    //   });
+    // notice.get().click(function() {
+    //   notice.remove();
+    // });
+
+    // var stickyNotice = new PNotify({
+    //   title: 'Sticky Notice',
+    //   text: 'Check me out! I\'m a sticky notice. You\'ll have to close me yourself.',
+    //   hide: false
+    // });
+    // notice.get().click(function() {
+    //   if ($(e.target).is('.ui-pnotify-closer *, .ui-pnotify-sticker * ')) return;
+    //   notice.remove();
+    // })
   });
+
+
+
 
 
 
--- Moneta/app/models/reminder.rb
+++ Moneta/app/models/reminder.rb
@@ -2,10 +2,4 @@ class Reminder < ActiveRecord::Base
 
   belongs_to :user
 
-  scope :todays_reminders, -> {
-
-     where('start_time=?', Time.now.beginning_of_day)
-  }
-
-
 end

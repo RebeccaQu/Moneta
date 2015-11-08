b3cfcd50e14932ea5f6acd6fe88dc0a2e3a9f852
Rebecca Qu <beks.qu@gmail.com>
2015-11-08 12:42:45 -0500

    populated seeds

--- Moneta/db/seeds.rb
+++ Moneta/db/seeds.rb
@@ -27,12 +27,72 @@ r1 = Reminder.create!(
   )
 
 r2 = Reminder.create!(
-  reminder_name: 'Algonquin Trip',
-  date: DateTime.strptime("11/20/2015", "%m/%d/%Y"),
-  start_time: DateTime.strptime("12:00", "%H:%M"),
-  duration: 86400,
-  repetition: 'none',
+  reminder_name: 'Call Michael Smith(son)! It\'s his birthday!' ,
+  date: DateTime.strptime("11/22/2015", "%m/%d/%Y"),
+  start_time: DateTime.strptime("9:00", "%H:%M"),
+  repetition: 'weekly',
   notification: false,
+  completion: false, 
+  user_id: u1.id
+  )
+
+r3 = Reminder.create!(
+  reminder_name: 'Take Alendronate',
+  date: DateTime.strptime("11/01/2015", "%m/%d/%Y"),
+  start_time: DateTime.strptime("6:00", "%H:%M"),
+  repetition: 'weekly',
+  notification: false,
+  completion: false, 
+  user_id: u1.id
+  )
+
+r4 = Reminder.create!(
+  reminder_name: 'Take Alendronate',
+  date: DateTime.strptime("11/08/2015", "%m/%d/%Y"),
+  start_time: DateTime.strptime("6:00", "%H:%M"),
+  repetition: 'weekly',
+  notification: false,
+  completion: false, 
+  user_id: u1.id
+  )
+
+r5 = Reminder.create!(
+  reminder_name: 'Take Alendronate',
+  date: DateTime.strptime("11/15/2015", "%m/%d/%Y"),
+  start_time: DateTime.strptime("6:00", "%H:%M"),
+  repetition: 'weekly',
+  notification: false,
+  completion: false, 
+  user_id: u1.id
+  )
+
+r6 = Reminder.create!(
+  reminder_name: 'Take Alendronate',
+  date: DateTime.strptime("11/22/2015", "%m/%d/%Y"),
+  start_time: DateTime.strptime("6:00", "%H:%M"),
+  repetition: 'weekly',
+  notification: false,
+  completion: false, 
+  user_id: u1.id
+  )
+
+r7 = Reminder.create!(
+  reminder_name: 'Take Alendronate',
+  date: DateTime.strptime("11/29/2015", "%m/%d/%Y"),
+  start_time: DateTime.strptime("6:00", "%H:%M"),
+  repetition: 'weekly',
+  notification: false,
+  completion: false, 
+  user_id: u1.id
+  )
+
+r8 = Reminder.create!(
+  reminder_name: 'Laundry',
+  date: DateTime.strptime("11/08/2015", "%m/%d/%Y"),
+  start_time: DateTime.strptime("22:00", "%H:%M"),
+  duration: 108,
+  repetition: 'weekly',
+  notification: true,
   completion: false, 
   user_id: u1.id
   )

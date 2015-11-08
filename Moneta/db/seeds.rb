c25fd1d7ae8fbd8a0f6b05c2b7a36b986ac42b2c
Rebecca Qu <beks.qu@gmail.com>
2015-11-08 15:14:40 -0500

    seeded stuff, fixed styling stuff

--- Moneta/db/seeds.rb
+++ Moneta/db/seeds.rb
@@ -18,7 +18,7 @@ c1 = Caregiver.create!(
 r1 = Reminder.create!(
   reminder_name: 'Laundry',
   date: DateTime.strptime("11/08/2015", "%m/%d/%Y"),
-  start_time: DateTime.strptime("22:00", "%H:%M"),
+  start_time: DateTime.strptime("11:00", "%H:%M"),
   duration: 108,
   repetition: 'weekly',
   notification: true,
@@ -26,20 +26,10 @@ r1 = Reminder.create!(
   user_id: u1.id
   )
 
-r2 = Reminder.create!(
-  reminder_name: 'Call Michael Smith(son)! It\'s his birthday!' ,
-  date: DateTime.strptime("11/22/2015", "%m/%d/%Y"),
-  start_time: DateTime.strptime("9:00", "%H:%M"),
-  repetition: 'weekly',
-  notification: false,
-  completion: false, 
-  user_id: u1.id
-  )
-
 r3 = Reminder.create!(
   reminder_name: 'Take Alendronate',
   date: DateTime.strptime("11/01/2015", "%m/%d/%Y"),
-  start_time: DateTime.strptime("6:00", "%H:%M"),
+  start_time: DateTime.strptime("18:00", "%H:%M"),
   repetition: 'weekly',
   notification: false,
   completion: false, 
@@ -49,7 +39,7 @@ r3 = Reminder.create!(
 r4 = Reminder.create!(
   reminder_name: 'Take Alendronate',
   date: DateTime.strptime("11/08/2015", "%m/%d/%Y"),
-  start_time: DateTime.strptime("6:00", "%H:%M"),
+  start_time: DateTime.strptime("18:00", "%H:%M"),
   repetition: 'weekly',
   notification: false,
   completion: false, 
@@ -59,7 +49,7 @@ r4 = Reminder.create!(
 r5 = Reminder.create!(
   reminder_name: 'Take Alendronate',
   date: DateTime.strptime("11/15/2015", "%m/%d/%Y"),
-  start_time: DateTime.strptime("6:00", "%H:%M"),
+  start_time: DateTime.strptime("18:00", "%H:%M"),
   repetition: 'weekly',
   notification: false,
   completion: false, 
@@ -69,7 +59,7 @@ r5 = Reminder.create!(
 r6 = Reminder.create!(
   reminder_name: 'Take Alendronate',
   date: DateTime.strptime("11/22/2015", "%m/%d/%Y"),
-  start_time: DateTime.strptime("6:00", "%H:%M"),
+  start_time: DateTime.strptime("18:00", "%H:%M"),
   repetition: 'weekly',
   notification: false,
   completion: false, 
@@ -79,21 +69,41 @@ r6 = Reminder.create!(
 r7 = Reminder.create!(
   reminder_name: 'Take Alendronate',
   date: DateTime.strptime("11/29/2015", "%m/%d/%Y"),
-  start_time: DateTime.strptime("6:00", "%H:%M"),
+  start_time: DateTime.strptime("18:00", "%H:%M"),
   repetition: 'weekly',
   notification: false,
   completion: false, 
   user_id: u1.id
   )
 
-r8 = Reminder.create!(
-  reminder_name: 'Laundry',
+r10 = Reminder.create!(
+  reminder_name: 'Call Michael Smith(son)! It\'s his birthday!' ,
   date: DateTime.strptime("11/08/2015", "%m/%d/%Y"),
   start_time: DateTime.strptime("22:00", "%H:%M"),
-  duration: 108,
   repetition: 'weekly',
-  notification: true,
+  notification: false,
   completion: false, 
   user_id: u1.id
   )
 
+m1 = Medication.create!(
+  medication_name: "Glargine", 
+  brand: "Lantus", 
+  dose: "10 U", 
+  route: "SC",
+  frequency: "QHS", 
+  refill_date: DateTime.strptime("08/19/2015", "%m/%d/%Y"), 
+  refill_days: 30, 
+  prescriber: "Dr. Jay Ponka",
+  dispenser: "Rebecca Seale", 
+  diagnosis: "diabetes"
+  )
+
+f1 = Family.create!(
+  first_name: "Michael", 
+  last_name: "Smith", 
+  dob: "11.08.1980", 
+  email: "michaels@gmail.com", 
+  phone_number: "4169806743", 
+  relationship: "son"
+  )
